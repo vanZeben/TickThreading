fc81b625313a7e5cc1626e63a834dba3df65f1bf
vanZeben <ryan@vanZeben.ca>
2014-05-08 19:28:05 -0400

    Update to reference external patches

--- src/main/java/nallar/tickthreading/patcher/PatchHook.java
+++ src/main/java/nallar/tickthreading/patcher/PatchHook.java
@@ -1,8 +1,15 @@
 package nallar.tickthreading.patcher;
 
-import nallar.log.PatchLog;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.lang.reflect.Field;
 
-import java.lang.reflect.*;
+import nallar.log.PatchLog;
 
 public class PatchHook {
 	private static Patcher patcher;
@@ -20,13 +27,52 @@ public class PatchHook {
 			PatchLog.severe("Failed to set up MCPC+ startup args. This is only a problem if you are using MCPC+", t);
 		}
 		try {
-			patcher = new Patcher(PatchLauncher.class.getResourceAsStream("/patches.xml"), Patches.class);
+			patcher = new Patcher(getPatchFileStream(), Patches.class);
 		} catch (Throwable t) {
 			PatchLog.severe("Failed to create Patcher", t);
 			System.exit(1);
 		}
 	}
 
+	public static FileInputStream getPatchFileStream() {
+	  File file = new File("./config/TickThreadingPatches.xml");
+	  if (!file.exists()) {
+	    InputStream stream = null;
+	    OutputStream resStreamOut = null;
+	    try {
+	      file.createNewFile();
+	      stream = PatchLauncher.class.getResourceAsStream("/patches.xml");
+	      if (stream == null) {
+	        return new FileInputStream(file);
+	      }
+	      int readBytes;
+	      byte[] buffer = new byte[4096];
+	      resStreamOut = new FileOutputStream(file);
+	      while ((readBytes = stream.read(buffer)) > 0) {
+	        resStreamOut.write(buffer, 0, readBytes);
+	      }
+	    } catch (IOException e) {
+	      e.printStackTrace();
+	    } finally {
+	      try {
+	        if (stream != null) {
+	          stream.close();
+	        }
+	        if (resStreamOut != null) {
+	          resStreamOut.close();
+	        }
+	      } catch (IOException e) {
+	        e.printStackTrace();
+	      }
+	    }
+	  }
+	  try {
+	    return new FileInputStream(file);
+	  } catch (FileNotFoundException e) {
+	    return null;
+	  }
+	}
+	
 	public static byte[] preSrgTransformationHook(String name, String transformedName, byte[] originalBytes) {
 		try {
 			return patcher.preSrgTransformation(name, transformedName, originalBytes);
